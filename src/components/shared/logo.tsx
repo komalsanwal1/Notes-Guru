31989ff0ac0d71f9bf3bbc1d5eefe223db4a4afe
komalsanwal1 <komalsansanwal05@gmail.com>
2025-06-08 18:59:51 +0000

    location in not proper

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
 const Logo = () => (
-  <Link href="/" className="flex items-center gap-2 group" aria-label="NoteGuru Home">
+  <Link href="/" className="flex items-center gap-2 group mr-auto" aria-label="NoteGuru Home">
     <svg
       width="32"
       height="32"
